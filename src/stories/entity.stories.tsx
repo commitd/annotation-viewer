1c1e9c63ace10a4f930434b4edefd0ac3341ab77
Jonathan Elliot <jon@committed.io>
2019-09-03 10:40:45 +0100

    Story decorator

--- src/stories/entity.stories.tsx
+++ src/stories/entity.stories.tsx
@@ -2,17 +2,20 @@ import * as React from 'react'
 
 import { storiesOf } from '@storybook/react'
 import Entity from '../components/Entity'
+import { contained } from './decorators'
 
-storiesOf('entity', module).add('default', () => (
-  <Entity
-    annotation={{
-      offset: 49,
-      length: 10,
-      label: 'HM Parliament',
-      annotationType: 'entity',
-      entityType: 'org'
-    }}
-  >
-    Parliament
-  </Entity>
-))
+storiesOf('entity', module)
+  .addDecorator(contained)
+  .add('default', () => (
+    <Entity
+      annotation={{
+        offset: 49,
+        length: 10,
+        label: 'HM Parliament',
+        annotationType: 'entity',
+        entityType: 'org'
+      }}
+    >
+      Parliament
+    </Entity>
+  ))
