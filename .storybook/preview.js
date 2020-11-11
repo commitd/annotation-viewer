8da94e7d920cfb37ab4d3c5c3c7b7c1ba7a799a2
Jonathan Elliot <jon@committed.io>
2020-11-11 12:10:18 +0000

    Wrap stories in a ThemeProvider and add dark theme support

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,9 @@
+import { withTheme } from './withTheme'
+
 // https://storybook.js.org/docs/react/writing-stories/parameters#global-parameters
 export const parameters = {
   // https://storybook.js.org/docs/react/essentials/actions#automatically-matching-args
   actions: { argTypesRegex: '^on.*' },
-};
+}
+
+export const decorators = [withTheme]
